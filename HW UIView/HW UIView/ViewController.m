d9063692ab7fb3c8a0c0c03ff64a96a94bcc4be5
Кирилл Федоров <ikirillfedorov@gmail.com>
2019-03-05 19:32:29 +0300

    HW UIView fixing#2

--- HW UIView/HW UIView/ViewController.m
+++ HW UIView/HW UIView/ViewController.m
@@ -28,8 +28,6 @@
     CGFloat width = self.view.bounds.size.width;
     CGFloat height = self.view.bounds.size.height;
     CGFloat minSide = MIN(width, height);
-    NSLog(@"rect = %@", NSStringFromCGRect(CGRectMake(0, 0, width, height)));
-    NSLog(@"x = %f y = %f", self.view.center.x, self.view.center.x);
     NSInteger cellsInLine = 20;
     
     self.field = [[UIView alloc] initWithFrame:CGRectMake(self.view.center.x - minSide / 2, self.view.center.y - minSide / 2, minSide, minSide)];
@@ -41,13 +39,15 @@
     CGFloat checkerSize = self.cellSize * multiplier;
     CGFloat indentInCell = self.cellSize * (1 - multiplier) / 2;
     NSInteger linesWithChekers = 16;
+    NSInteger cellsWithOutChekers = cellsInLine - linesWithChekers / 2;
     
     for (NSInteger i = 0; i < cellsInLine; i++) {
         for (NSInteger j = 0; j < cellsInLine; j++) {
+            BOOL isEven = (i + j) % 2 == 0;
             
             UIView *cell = [[UIView alloc]initWithFrame:CGRectMake(self.cellSize * j, self.cellSize * i, self.cellSize, self.cellSize)];
             
-            if ((i + j) % 2 != 0) {
+            if (!isEven) {
                 cell.backgroundColor = [UIColor lightGrayColor];
             } else {
                 cell.backgroundColor = [UIColor blackColor];
@@ -59,10 +59,10 @@
                                                                           checkerSize,
                                                                           checkerSize)];
 
-            if (i < cellsInLine - (cellsInLine - (linesWithChekers / 2)) && (i + j) % 2 == 0) {
+            if (i < cellsInLine - cellsWithOutChekers && isEven) {
                 checker.backgroundColor = [UIColor whiteColor];
             }
-            if (i >= cellsInLine - linesWithChekers / 2 && (i + j) % 2 == 0) {
+            if (i >= cellsWithOutChekers && isEven) {
                 checker.backgroundColor = [UIColor redColor];
             }
             [self.field addSubview:checker];
@@ -74,10 +74,10 @@
 //    3. Доска должна быть вписана в максимально возможный квадрат, т.е. либо бока, либо верх или низ должны касаться границ экрана
 //    4. Применяя соответствующие маски сделайте так, чтобы когда устройство меняет ориентацию, то все клетки растягивались соответственно и ничего не вылетало за пределы экрана.
 
-    self.field.autoresizingMask =   UIViewAutoresizingFlexibleTopMargin |
-                                    UIViewAutoresizingFlexibleBottomMargin |
-                                    UIViewAutoresizingFlexibleLeftMargin |
-                                    UIViewAutoresizingFlexibleRightMargin;
+    self.field.autoresizingMask = UIViewAutoresizingFlexibleTopMargin |
+                                  UIViewAutoresizingFlexibleBottomMargin |
+                                  UIViewAutoresizingFlexibleLeftMargin |
+                                  UIViewAutoresizingFlexibleRightMargin;
     
     //Мастер
     //5. При повороте устройства все черные клетки должны менять цвет :)
@@ -111,20 +111,21 @@
     //8. Поставьте белые и красные шашки (квадратные вьюхи) так как они стоят на доске. Они должны быть сабвьюхами главной вьюхи (у них и у клеток один супервью)
     //9. После каждого переворота шашки должны быть перетасованы используя соответствующие методы иерархии UIView
 
-    NSInteger randomCountChange = arc4random() % ([self.checkersArray count] / 2) + 1;
-
-    for (NSInteger i = 0; i < randomCountChange; i++) {
-
-        [UIView animateWithDuration:1 animations:^{
-
-            CGRect tempRect = self.randomChecker.frame;
-            [self.randomChecker setFrame:self.randomChecker.frame];
-            [self.randomChecker setFrame:tempRect];
-
-            [self.field bringSubviewToFront:self.randomChecker];
-            [self.field bringSubviewToFront:self.randomChecker];
-        }];
-    }
+    [UIView animateWithDuration:1 animations:^{
+        NSInteger randomCountChange = arc4random() % ([self.checkersArray count] / 2) + 1;
+        
+        for (NSInteger i = 0; i < randomCountChange; i++) {
+            UIView* firstChecker = [self randomChecker];
+            UIView* secondChecker = [self randomChecker];
+            
+            CGRect tempRect = firstChecker.frame;
+            [firstChecker setFrame:secondChecker.frame];
+            [secondChecker setFrame:tempRect];
+            
+            [self.field bringSubviewToFront:firstChecker];
+            [self.field bringSubviewToFront:secondChecker];
+        }
+    }];
 }
 
 @end
