01b51368834101a8422c5d662c103005e8cdd147
Кирилл Федоров <ikirillfedorov@gmail.com>
2019-03-10 14:16:05 +0300

    Add HW20 IBOutlet - fixing #2
    Add HW20 IBOutlet - fixing #2

--- HW OutletTest/OutletTest/ViewController.swift
+++ HW OutletTest/OutletTest/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+var blackCheckers = [UIView]()
 
 class ViewController: UIViewController {
     
@@ -18,30 +19,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        for cell in cells {
+            if cell.backgroundColor == .black {
+                blackCheckers.append(cell)
+            }
+        }
+        
     }
     
     override func willTransition(to newCollection: UITraitCollection,
                                  with coordinator: UIViewControllerTransitionCoordinator) {
         
         let randomColor =  UIColor(red: CGFloat.random(in: 0...1),
-                                    green: CGFloat.random(in: 0...1),
-                                    blue: CGFloat.random(in: 0...1),
-                                    alpha: 1)
-
-        for cell in cells {
-            if cell.backgroundColor != UIColor.white {
-                cell.backgroundColor = randomColor
-            }
+                                   green: CGFloat.random(in: 0...1),
+                                   blue: CGFloat.random(in: 0...1),
+                                   alpha: 1)
+        
+        for checker in blackCheckers {
+            checker.backgroundColor = randomColor
         }
         
         let randomCountChange = (Int.random(in: 0...Int.max) % self.checkers.count / 2) + 1
-
+        
         UIView.animate(withDuration: 1) {
             
             for _ in 0...randomCountChange {
                 let firstChecker = self.checkers[Int.random(in: 0..<self.checkers.count)]
                 let secondChecker = self.checkers[Int.random(in: 0..<self.checkers.count)]
-
+                
                 let tempRect = firstChecker.frame
                 firstChecker.frame = secondChecker.frame
                 secondChecker.frame = tempRect
@@ -52,4 +57,5 @@ class ViewController: UIViewController {
         }
     }
 }
+
 
