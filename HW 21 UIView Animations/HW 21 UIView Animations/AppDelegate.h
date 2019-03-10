e929a2b60cd4ccb8dea2e5312dd0668c4b177085
Кирилл Федоров <ikirillfedorov@gmail.com>
2019-03-10 13:05:48 +0300

    Add HW 21 UIView Animations
    Add HW 21 UIView Animations

---
+++ HW 21 UIView Animations/HW 21 UIView Animations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HW 21 UIView Animations
+//
+//  Created by Kirill Fedorov on 09/03/2019.
+//  Copyright © 2019 Kirill Fedorov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
