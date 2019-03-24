e2d044ea76eea27ee07999c10022bb711fca0e3c
Кирилл Федоров <ikirillfedorov@gmail.com>
2019-03-24 20:08:51 +0300

    Add HW 23

---
+++ HW 23 Gestures/HW 23 Gestures/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW 23 Gestures
+//
+//  Created by Kirill Fedorov on 21/03/2019.
+//  Copyright © 2019 Kirill Fedorov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
